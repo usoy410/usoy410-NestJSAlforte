16435dab32771e0d4c887e48f6a7a644ff8b1cb9
Usoy410 <aralforte@gbox.ncf.edu.ph>
2025-12-03 17:20:25 +0800

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
-// i imported the position module 
+// i imported the position module
 import { Module } from "@nestjs/common";
 import { AuthModule } from "./auth/auth.modules";
 import { DatabaseModule } from "./database/database.module";
 import { EventsModule } from "./events/events.module";
+import { NotesModule } from "./notes/notes.module";
 import { PositionsModule } from "./positions/positions.module";
 import { UsersModule } from "./users/users.module";
 @Module({
-    imports: [DatabaseModule, UsersModule, AuthModule, EventsModule, PositionsModule],
+    imports: [DatabaseModule, UsersModule, AuthModule, EventsModule, NotesModule, PositionsModule],
 })
 export class AppModule {}
